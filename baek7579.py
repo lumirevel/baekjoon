ff68be3b05347cb8f32b1ee30a45cb238e46ad52
김령교 <horizonspluto@gmail.com>
2022-12-29 15:20:45 +0900

    앱(7579번)을 추가함

---
+++ baek7579.py
@@ -0,0 +1,7 @@
+from sys import stdin
+N, M = map(int, stdin.readline().split(" "))
+ms = list(map(int, stdin.readline().split(" ")))
+cs = list(map(int, stdin.readline().split(" ")))
+
+DP = [[] for ]
+for n
