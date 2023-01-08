68987253ca03d053a38557e60dfc38877227b919
김령교 <horizonspluto@gmail.com>
2023-01-09 04:10:43 +0900

    pypy3로 했을 때의 런타임에러는 인덱스 에러로 rear_sum_count의 크기가 작아서 넘쳐서 생긴 일이었다. 크기를 두 배로 늘리니 성공했다.

--- baek1208.py
+++ baek1208.py
@@ -4,7 +4,7 @@ nums = list(map(int, stdin.readline().split(" ")))
 
 front_sum = [None for _ in range(2 ** (N // 2))]
 rear_sum = [None for _ in range(2 ** (N - N // 2))]
-rear_sum_count = [0 for _ in range(2*20*100000+1)]
+rear_sum_count = [0 for _ in range(2*40*100000+1)]
 
 for i in range(2**(N//2)):
     if i == 0:
@@ -26,11 +26,11 @@ for i in range(2**(N-N//2)):
             n += 1
             q //= 2
         rear_sum[i] = rear_sum[i - 2 ** n] + nums[N // 2 + n]
-        rear_sum_count[rear_sum[i] + 20 * 100000] += 1
+        rear_sum_count[rear_sum[i] + 40 * 100000] += 1
 
 count_sum = 0
 for i, v in enumerate(front_sum):
     if i != 0 and v == S:# 공집합과 공집합의 결합은 막고 rear의 공집합을 적재적소에 쓴다.
         count_sum += 1
-    count_sum += rear_sum_count[S-v+20*100000]
+    count_sum += rear_sum_count[S-v+40*100000]
 print(count_sum)
