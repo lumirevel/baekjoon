731a85b655c18b1e02bb75c3ae08b873ad63370e
김령교 <horizonspluto@gmail.com>
2023-01-09 03:29:36 +0900

    공집합의 케이스를 제대로 분류함.

--- baek1208.py
+++ baek1208.py
@@ -19,7 +19,6 @@ for i in range(2**(N//2)):
 for i in range(2**(N-N//2)):
     if i == 0:
         rear_sum[i] = 0
-        rear_sum_count[i] += 1
     else:
         q = i
         n = -1
@@ -27,9 +26,11 @@ for i in range(2**(N-N//2)):
             n += 1
             q //= 2
         rear_sum[i] = rear_sum[i - 2 ** n] + nums[N // 2 + n]
-        rear_sum_count[rear_sum[i] + 20 * 100000] = rear_sum_count[rear_sum[i - 2 ** n] + 20 * 100000]
+        rear_sum_count[rear_sum[i] + 20 * 100000] += 1
 
 count_sum = 0
-for v in front_sum:
-    count_sum += rear_sum_count[S-v]
+for i, v in enumerate(front_sum):
+    if i != 0 and v == S:# 공집합과 공집합의 결합은 막고 rear의 공집합을 적재적소에 쓴다.
+        count_sum += 1
+    count_sum += rear_sum_count[S-v+20*100000]
 print(count_sum)
