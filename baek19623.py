180b67606fa0d2808ef9a7c3cb710896c2cbc48d
김령교 <horizonspluto@gmail.com>
2023-01-15 21:21:49 +0900

    회의실 배정 4(19623번)를 해결함. P의 구현을 list로 바꾸고 P를 만드는 방식을 O(N)으로 만들어 해결했다

--- baek19623.py
+++ baek19623.py
@@ -1,31 +1,46 @@
 from sys import stdin
 N = int(stdin.readline())
+
+class Meet:
+    def __init__(self, s, e, w):
+        self.s = s
+        self.e = e
+        self.w = w
+        self.i = None
+    def __repr__(self):
+        return f"{self.s} {self.e} {self.w}"
+
+
 meeting = []
 for _ in range(N):
-    meeting.append(tuple(map(int, stdin.readline().split(" "))))
-meeting.sort(key=lambda x:x[1])
+    s, e, w = map(int, stdin.readline().split(" "))
+    meeting.append(Meet(s,e,w))
+meeting.sort(key=lambda x:x.e)
+
+sort_by_start_meeting = []
+for i, v in enumerate(meeting):
+    v.i = i
+    sort_by_start_meeting.append(v)
+sort_by_start_meeting.sort(key=lambda x:x.s)
+
+P = [None for _ in range(N)]
+i, j = N - 1, N - 1
+while j >= 0:
+    while i != 0 and meeting[i].e > sort_by_start_meeting[j].s:
+        i -= 1
+    if meeting[i].e <= sort_by_start_meeting[j].s:
+        P[sort_by_start_meeting[j].i] = i
+    j -= 1
 
 DP = []
-
-
-def P(j):
-    i = j - 1
-    while i != -1 and meeting[i][1] > meeting[j][0]:
-        i -= 1
-    if i < 0:
-        return None
-    else:
-        return i
-
-
 for j, meet in enumerate(meeting):
     if j == 0:
-        DP.append(meet[2])
+        DP.append(meet.w)
     else:
-        i = P(j)
+        i = P[j]
         if i is not None:
-            DP.append(max(DP[j - 1], DP[i] + meet[2]))
+            DP.append(max(DP[j - 1], DP[i] + meet.w))
         else:
-            DP.append(max(DP[j - 1], meet[2]))
+            DP.append(max(DP[j - 1], meet.w))
 
 print(DP[-1])
