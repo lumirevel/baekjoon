d43450509f1b175094b4aabfb372df66664abea8
김령교 <horizonspluto@gmail.com>
2023-01-15 17:32:43 +0900

    Discrete Mathematics에서 배운 대로 했는데 worst case가 O(N^2)까지도 나올 수 있고 그러면 10^10이 나와 TLE가 난 듯하다

--- baek19623.py
+++ baek19623.py
@@ -3,3 +3,29 @@ N = int(stdin.readline())
 meeting = []
 for _ in range(N):
     meeting.append(tuple(map(int, stdin.readline().split(" "))))
+meeting.sort(key=lambda x:x[1])
+
+DP = []
+
+
+def P(j):
+    i = j - 1
+    while i != -1 and meeting[i][1] > meeting[j][0]:
+        i -= 1
+    if i < 0:
+        return None
+    else:
+        return i
+
+
+for j, meet in enumerate(meeting):
+    if j == 0:
+        DP.append(meet[2])
+    else:
+        i = P(j)
+        if i is not None:
+            DP.append(max(DP[j - 1], DP[i] + meet[2]))
+        else:
+            DP.append(max(DP[j - 1], meet[2]))
+
+print(DP[-1])
