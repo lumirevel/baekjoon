ff6b51094c98e7bdad10840145a500d0f7e02e95
김령교 <horizonspluto@gmail.com>
2023-02-09 13:52:00 +0900

    수업(19700)을 제출했는데 2%에서 '틀렸습니다'가 떴다.

--- baek19700.py
+++ baek19700.py
@@ -1,8 +1,34 @@
 from sys import stdin
+max_N = 500000
 N = int(stdin.readline())
-p = []
+people = []
 for _ in range(N):
-    p.append(tuple(map(int, stdin.readline().split(" "))))
+    people.append(tuple(map(int, stdin.readline().split(" "))))
+people.sort(key=lambda x:x[0], reverse=True)
 
-p.sort(key=lambda x:x[0])
-print(p)
+teams = []
+position = [None for _ in range(max_N)]
+for person in people:
+    if teams:
+        length = min(person[1] - 1, len(teams[0]))
+        while length and position[length] is None:
+            length -= 1
+        if length == 0:
+            teams.append([person])
+            if position[1] is None:
+                position[1] = len(teams) - 1
+        else:
+            teams[position[length]].append(person)
+            if position[length] == len(teams) - 1:
+                position[length] = None
+            elif len(teams[position[length] + 1]) != length:
+                position[length] = None
+            else:
+                if position[length + 1] is None:
+                    position[length + 1] = position[length]
+                position[length] += 1
+    else:
+        teams.append([person])
+        position[1] = 0
+
+print(len(teams))
