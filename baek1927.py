ffca6f821bb083b65d90b8cb2e7c6504d91feb68
김령교 <horizonspluto@gmail.com>
2023-03-13 16:45:50 +0900

    최소 힙(1927번) pop을 제외하고 모두 구현함

--- baek1927.py
+++ baek1927.py
@@ -3,17 +3,50 @@ N = int(stdin.readline())
 
 class Heap:
     def __init__(self):
-        self.n = 0
-        self.tree = [0]
+        self.__N = 0
+        self.tree = [None]
     def append(self, a):
-        self.n += 1
-        2*n, 2*n+1
+        self.__N += 1
+        if self.__N < len(self.tree):
+            self.tree[self.__N] = a
+        else:
+            self.tree.append(a)
 
-    def pop(self):
-        if self.n:
-            pass
+        p = self.__N // 2
+        i = self.__N
+        while p != 0 and self.tree[p] > self.tree[i]:
+            self.tree[p], self.tree[i] = self.tree[i], self.tree[p]
+            i = p
+            p //= 2
+
+    def pop(self):#is not implemented completely
+        if self.__N:
+            m = self.tree[1]
+            i = 1
+            while 2*i < self.__N:
+                if self.tree[2*i] < self.tree[2*i+1]:
+                    self.tree[i], self.tree[2*i] = self.tree[2*i], self.tree[i]
+                    i = 2*i
+                else:
+                    self.tree[i], self.tree[2 * i + 1] = self.tree[2 * i + 1], self.tree[i]
+                    i = 2*i+1
+
+            if self.__N <= 2 * i:
+                self.tree[i], self.tree[2 * i] = self.tree[2 * i], self.tree[i]
+            elif self.__N <= 2*i+1:
+                if self.tree[2 * i] < self.tree[2 * i + 1]:
+                    self.tree[i], self.tree[2 * i] = self.tree[2 * i], self.tree[i]
+                    self.tree[2*i], self.tree[2*i+1] = self.tree[2*i+1], self.tree[2*i]
+                else:
+                    self.tree[i], self.tree[2 * i + 1] = self.tree[2 * i + 1], self.tree[i]
+            self.tree.pop()
+            self.__N -= 1
+            return m
         else:
             return 0
+
+    def __len__(self):
+        return self.__N
 
 heap = Heap()
 
@@ -23,3 +56,5 @@ for _ in range(N):
         heap.append(x)
     else:
         print(heap.pop())
+
+print(heap.tree[1])
