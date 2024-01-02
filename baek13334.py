e3564e9d6d351128de0ae2c537dbfd14f95572a3
김령교 <horizonspluto@gmail.com>
2024-01-02 18:46:26 +0900

    철로(13334번)를 해결함. sweeping을 적용하여 성공함

--- baek13334.py
+++ baek13334.py
@@ -1,37 +1,29 @@
 from sys import stdin
-from collections import deque
-from heapq import heapify, heappush, heappop
+from queue import PriorityQueue
 
-N = int(stdin.readline())
+N = int(input())
+
 lines = []
-startPoint = set()
 for _ in range(N):
     ho = list(map(int, stdin.readline().split(" ")))
     lines.append((min(ho), max(ho)))
-D = int(stdin.readline())
-# 특정 구간에 온전한 ho가 몇 개 있는 지를 알아야 함->func으로 만들 것
-# 아이디어 1: line를 반전시키면?(ho에서 철로 선분을 검증하면?) O(n²)이다...
-# 아이디어 2: line가 적용되는 시작점의 범위를 각각 구한다
-# -> 이미 있는 시작점을 저장한 set의 항목들을 철로 선분 시작점으로 잡는다
-# -> 각 경우의 겹치는 수를 계산하여 max와 비교해 업데이트한다.
-# 이 역시 worst가 O(n²)이다...
-# 아이디어 3: elevator 기법!!!
-startPoint = set()
-for s,e in lines:
-    startPoint.add(s)
-    startPoint.add(e-D)
-startPoint = list(startPoint)
-startPoint.sort()
+lines.sort(key = lambda x:x[1])
+D = int(input())
 
-M = 0
-heapify(lines)
-elevator = deque()
-cache = deque()
-for s in startPoint:
-    while elevator and elevator[0][0] < s:
-        elevator.popleft()
-    while cache and cache:
-        pass
-    
-    if M < len(elevator):
-        M = len(elevator)
+# 집어 넣고
+# 거리를 보면서 뺀다
+
+# 끝을 기준으로 정렬하고
+# 하나씩 빼면서
+# 시작점을 기준으로 heap 사용
+maxPeople = 0
+onRail = PriorityQueue()
+for line in lines:
+    if line[1] - line[0] <= D:
+        if not onRail.empty() and onRail.queue[0] < line[1] - D:
+            onRail.get()
+        onRail.put(line[0])
+    if maxPeople < onRail.qsize():
+        maxPeople = onRail.qsize()
+
+print(maxPeople)
