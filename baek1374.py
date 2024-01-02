7838399ab65f7c7108aa0dbdcd3431f0eac8e7a5
김령교 <horizonspluto@gmail.com>
2024-01-02 14:40:02 +0900

    작성한 sweeping이 잘못되었음을 깨닫고 PriorityQueue를 활용해 다시 작성했으나 실패함

--- baek1374.py
+++ baek1374.py
@@ -1,20 +1,20 @@
+from queue import PriorityQueue
+
 N = int(input())
 
 lectures = []
 for _ in range(N):
     lectures.append(tuple(map(int, input().split(" "))))
-lectures.sort(key = lambda x:x[2], reverse = True)
 lectures.sort(key = lambda x:x[1])
 
-endPoint = -1
-duple = []
+requirement = 0
+remnant = PriorityQueue()
 for lecture in lectures:
-    if endPoint < lecture[1]:
-        endPoint = lecture[2]
-        duple.append(0)
-    elif lecture[2] < endPoint:
-        endPoint = lecture[2]
-    duple[-1]+=1
+    while (not remnant.empty() and remnant.queue[0] < lecture[1]):
+        remnant.get()
+    remnant.put(lecture[2])
 
-duple.sort()
-print(duple[-1])
+    if requirement < remnant.qsize():
+        requirement = remnant.qsize()
+
+print(requirement)
