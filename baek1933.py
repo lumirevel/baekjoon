00319b4c7cca9478afc968d9a16a42791a148a93
김령교 <horizonspluto@gmail.com>
2024-01-03 18:50:27 +0900

    오래 고민했다. 머리 깎으러 걸어가며 아파트를 보고 방법을 떠올렸다. L의 크기 순으로 비교하며 H를 max heap에 저장하고 꺼내며 R을 비교하는 방법을 고안해냈다. 하지만 틀렸다. 하지만 코드를 고치면 될 것 같다.

--- baek1933.py
+++ baek1933.py
@@ -1,5 +1,37 @@
 from sys import stdin
+from queue import PriorityQueue
+
 N = int(stdin.readline())
-building = []
+
+buildings = [(1000000001,0,1000000001)]
 for _ in range(N):
-    building.append(list(map(int, stdin.readline().split(" "))))
+    buildings.append(tuple(map(int, stdin.readline().split(" "))))
+buildings.sort(key = lambda x:x[0])
+
+tray = PriorityQueue()
+changes = PriorityQueue()
+for building in buildings:
+    L, H, R = building
+    if not tray.empty():
+        maxR = tray.queue[0][1]
+        while not tray.empty() and tray.queue[0][1] < L:
+            nowMH, nowR = tray.get()
+
+            if maxR < nowR:
+                changes.put((maxR, -nowMH))
+                maxR = nowR
+
+        if tray.empty():
+            changes.put((maxR, 0))
+        elif maxR != tray.queue[0][1]:
+            changes.put((maxR, -tray.queue[0][0]))
+
+    tray.put((-H,R))
+    if tray.queue[0][0] != 0 and -tray.queue[0][0] == H:
+        changes.put((L, H))
+
+result = "basement: "
+while(not changes.empty()):
+    change = changes.get()
+    result += f"{change[0]} {change[1]} "
+print(result[0:-1])
