3b4dc53d055f29cb7e66c672e53429bc767d0b03
김령교 <horizonspluto@gmail.com>
2024-01-03 19:02:58 +0900

    코드를 조금 수정했다. 뒤늦게 마지막 result에 "basement"라는 글이 적힌다는 걸 깨달았고 수정했다. 이번에도 실패했다.

--- baek1933.py
+++ baek1933.py
@@ -3,13 +3,13 @@ from queue import PriorityQueue
 
 N = int(stdin.readline())
 
-buildings = [(1000000001,0,1000000001)]
+buildings = []
 for _ in range(N):
     buildings.append(tuple(map(int, stdin.readline().split(" "))))
 buildings.sort(key = lambda x:x[0])
 
 tray = PriorityQueue()
-changes = PriorityQueue()
+changes = []
 for building in buildings:
     L, H, R = building
     if not tray.empty():
@@ -17,21 +17,32 @@ for building in buildings:
         while not tray.empty() and tray.queue[0][1] < L:
             nowMH, nowR = tray.get()
 
-            if maxR < nowR:
-                changes.put((maxR, -nowMH))
+            if maxR < nowR:# 떨어져 도착한 높이(-nowMH)를 떨어진 위치(maxR)로 기록해야함
+                changes.append((maxR, -nowMH))
                 maxR = nowR
 
-        if tray.empty():
-            changes.put((maxR, 0))
+        if tray.empty():# 떨어졌는데 바닥이 없는 경우
+            changes.append((maxR, 0))
         elif maxR != tray.queue[0][1]:
-            changes.put((maxR, -tray.queue[0][0]))
+            changes.append((maxR, -tray.queue[0][0]))
 
     tray.put((-H,R))
-    if tray.queue[0][0] != 0 and -tray.queue[0][0] == H:
-        changes.put((L, H))
+    if -tray.queue[0][0] == H:
+        changes.append((L, H))
 
-result = "basement: "
-while(not changes.empty()):
-    change = changes.get()
+# 모두 뽑아야 하기 때문에
+maxR = tray.queue[0][1]
+while not tray.empty():
+    nowMH, nowR = tray.get()
+    if maxR < nowR:# 떨어져 도착한 높이(-nowMH)를 떨어진 위치(maxR)로 기록해야함
+        changes.append((maxR, -nowMH))
+        maxR = nowR
+if tray.empty():# 떨어졌는데 바닥이 없는 경우
+    changes.append((maxR, 0))
+elif maxR != tray.queue[0][1]:
+    changes.append((maxR, -tray.queue[0][0]))
+
+result = ""
+for change in changes:
     result += f"{change[0]} {change[1]} "
 print(result[0:-1])
