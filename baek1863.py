e6b514bfe0e246363656c5995908629aa4f5ffc0
김령교 <horizonspluto@gmail.com>
2024-01-04 00:33:37 +0900

    92%에서 틀렸다

---
+++ baek1863.py
@@ -0,0 +1,24 @@
+n = int(input())
+changes = []
+for _ in range(n):
+    changes.append(tuple(map(int, input().split(" "))))
+changes.sort(key = lambda x:x[0])
+
+# 올라가는 곳 # 내려가는 곳
+count = 0
+stack = []
+for change in changes:
+    x, y = change
+    if stack:
+        if stack[-1] < y:
+            stack.append(y)
+        else:
+            while stack and stack[-1] > y:
+                stack.pop()
+                count += 1
+            if y != 0 and (not stack or stack[-1] != y):
+                stack.append(y)
+    else:
+        stack.append(y)
+count += len(stack)
+print(count)
