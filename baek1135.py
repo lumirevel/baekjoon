de3fb86988141a5e3e457650fc33004afa89b041
김령교 <horizonspluto@gmail.com>
2024-01-07 01:59:41 +0900

    [#36]뉴스 전하기(1135번)를 해결함. timeDFS를 this를 root로 하는 subtree에서 전화에 걸리는 총 시간을 times에 저장하는 함수로 만듦. 전화 시간이 긴 subtree를 가진 inferior부터 고르는 것이 최소 전화 시간을 만든다는 loop invarient를 timeDFS 구현에 반영했고 맞았다.

--- baek1135.py
+++ baek1135.py
@@ -8,24 +8,16 @@ for i in range(N):
     if superiorOf[i] != -1:
         inferiorsOf[superiorOf[i]].append(i)
 
-count = []
+times = []
 for _ in range(N):
-    count.append(0)
-def countDFS(inferiors, node=0):
-    count[node] += len(inferiors[node])
-    for inferior in inferiors[node]:
-        count[node] += countDFS(inferiors, inferior)
-    return count[node]
+    times.append(0)
+def timeDFS(inferiors, this=0):
+    for inferior in inferiors[this]:
+        timeDFS(inferiors, inferior)
+    inferiors[this].sort(key = lambda x:times[x], reverse = True)
 
+    for i, inferior in enumerate(inferiors[this]):
+        times[this] = max(times[this], times[inferior]+i+1)
 
-def timeDFS(inferiors, node=0, time=0):
-    inferiors[node].sort(key=lambda x: count[x], reverse=True)
-
-    totalTime = time
-    for inferior in inferiors[node]:
-        time += 1
-        totalTime = max(totalTime, timeDFS(inferiors, inferior, time))
-    return totalTime
-
-countDFS(inferiorsOf)
-print(timeDFS(inferiorsOf))
+timeDFS(inferiorsOf)
+print(times[0])
