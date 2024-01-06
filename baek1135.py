7fdd0fe3671b48260b25c832312ee06b7fe29b43
김령교 <horizonspluto@gmail.com>
2024-01-07 00:59:59 +0900

    [#36]뉴스 전하기(1135번). 자식 노드를 root로 하는 subtree의 노드 수를 바탕으로 자식 노드 탐색의 우선 순위를 두는 방식으로 짰는데 틀림

---
+++ baek1135.py
@@ -0,0 +1,31 @@
+N = int(input())
+superiorOf = list(map(int, input().split(" ")))
+
+inferiorsOf = []
+for _ in range(N):
+    inferiorsOf.append([])
+for i in range(N):
+    if superiorOf[i] != -1:
+        inferiorsOf[superiorOf[i]].append(i)
+
+count = []
+for _ in range(N):
+    count.append(0)
+def countDFS(inferiors, node=0):
+    for inferior in inferiors[node]:
+        count[node] += len(inferiors[node])
+        count[node] += countDFS(inferiors, inferior)
+    return count[node]
+
+
+def timeDFS(inferiors, node=0, time=0):
+    inferiors[node].sort(key=lambda x: count[x], reverse=True)
+
+    totalTime = time
+    for inferior in inferiors[node]:
+        time += 1
+        totalTime = max(totalTime, timeDFS(inferiors, inferior, time))
+    return totalTime
+
+countDFS(inferiorsOf)
+print(timeDFS(inferiorsOf))
