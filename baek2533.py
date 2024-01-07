6c0bae9eea9de8b47756cc57aa2f05336a44529e
김령교 <horizonspluto@gmail.com>
2024-01-08 02:14:46 +0900

    [#43]사회망 서비스(SNS)(2533번). 2%에서 틀림

--- baek2533.py
+++ baek2533.py
@@ -1,20 +1,26 @@
-from sys import stdin, setrecursionlimit
-setrecursionlimit(1000000)
+from sys import stdin
 N = int(stdin.readline())
 
-tree = dict()
+adjList = []
+visited = []
 for _ in range(N):
+    adjList.append([])
+    visited.append(False)
+for _ in range(N-1):
     u, v = map(int, stdin.readline().rstrip().split(" "))
-    if u in tree.keys():
-        tree[u].append(v)
-    else:
-        tree[u] = [v]
-    if u in tree.keys():
-        tree[v].append(u)
-    else:
-        tree[v] = [u]
+    adjList[u - 1].append(v - 1)
 
-adaptors = []
+def bwMinColoredCount(adjList, node=0):
+    visited[node] = True
+    bMinColoredCount = 1
+    wMinColoredCount = 0
+    mustB = False
+    for child in adjList[node]:
+        if not visited[child]:
+            b,w = bwMinColoredCount(adjList, child)
+            bMinColoredCount += min(b,w)
+            wMinColoredCount += b
+    return bMinColoredCount, wMinColoredCount
 
-def DFS():
-    pass
+bMinColoredCount, wMinColoredCount = bwMinColoredCount(adjList)
+print(min(bMinColoredCount, wMinColoredCount))
