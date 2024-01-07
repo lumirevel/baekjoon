5fc81b8954f94bd4c785f61715f8712571e74a6a
김령교 <horizonspluto@gmail.com>
2024-01-07 20:52:49 +0900

    [#41]트리의 지름(1167번)을 해결함. toSecondMaximum 업데이트를 잘못하여 틀린 거였음. 발상은 맞았음

--- baek1167.py
+++ baek1167.py
@@ -17,9 +17,11 @@ for _ in range(V):
         toNode = say.popleft()
 
 # max와 max 직전
+# subMaximum = subTreeLengths에 들어온 node를 root로 한 subtree의 지름
+# toMaximum = subTreeLengths에 들어온 node를 root로 한 subtree의 leaf 노드에서 root 노드까지의 거리의 최댓값
 def subTreeLengths(adj, node=0):
     visited[node] = True
-    # 까지의 최대 거리와 subtree에서의 maxLength 둘 다 출격
+    # 까지의 최대 거리와 subtree에서의 maxLength 둘 다 출력
     subMaximum = 0
 
     toMaximum = 0
@@ -32,6 +34,8 @@ def subTreeLengths(adj, node=0):
             if toMaximum <= toMax + w:
                 toSecondMaximum = toMaximum
                 toMaximum = toMax + w
+            elif toSecondMaximum <= toMax + w:
+                toSecondMaximum = toMax + w
     if toSecondMaximum is None:
         return (toMaximum, max(subMaximum, toMaximum))
     else:
