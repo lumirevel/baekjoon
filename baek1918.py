27ebed02aef51cdff430a4c01a9ac88611f78c9a
김령교 <horizonspluto@gmail.com>
2024-01-08 19:44:06 +0900

    [#45]후위 표기식(1918번)을 해결함. 괄호 앞에 곱셈이 붙는 경우 해당 곱셈을 괄호식 변환 후 바로 후위 표기식으로 변환해야함을 놓침

--- baek1918.py
+++ baek1918.py
@@ -5,7 +5,7 @@ i = 0
 while i < len(notation):
     nowChar = notation[i]
     if nowChar == "(" or nowChar == "*" or nowChar == "/" or nowChar == "+" or nowChar == "-":
-        stack.append(nowChar)
+        pass
     elif nowChar == ")":
         smallStack = []
         while stack[-1] != "(":
@@ -16,15 +16,14 @@ while i < len(notation):
             number = smallStack.pop()
             nowChar = nowChar + number + operand
         stack.pop()
-        stack.append(nowChar)
-    else:
-        if stack:
+    if stack:
+        if nowChar != "(":
             prevChar = stack[-1]
             if prevChar == "*" or prevChar == "/":
                 operand = stack.pop()
                 number = stack.pop()
                 nowChar = number + nowChar + operand
-        stack.append(nowChar)
+    stack.append(nowChar)
     i += 1
 
 print(stack.pop())
