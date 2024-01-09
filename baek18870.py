a7d7b9693532bbb260fadfb27cce60a4c2f7b775
김령교 <horizonspluto@gmail.com>
2024-01-10 01:45:57 +0900

    [#50]좌표 압축(18870번). 여러 최적화를 적용했지만 시간초과 남

--- baek18870.py
+++ baek18870.py
@@ -1,16 +1,22 @@
+from sys import stdin
 N = int(input())
-coodinates = list(map(int, input().split(" ")))
+coodinates = list(map(int, stdin.readline().split(" ")))
 chart = list(set(coodinates.copy()))
 chart.sort()
 
-def findIn(chart, x, start = 0, end = len(chart)-1):
-    mid = (start+end)//2
-    if x < chart[mid]:
-        return findIn(chart, x, start = start, end=mid-1)
-    elif x > chart[mid]:
-        return findIn(chart, x, start = mid+1, end=end)
-    else:
-        return mid
+def findIn(chart, x):
+    start = 0
+    end = len(chart)-1
+    found = False
+    while not found:
+        mid = (start+end)//2
+        if x < chart[mid]:
+            end = mid-1
+        elif x > chart[mid]:
+            start = mid+1
+        else:
+            found = True
+    return mid
 
 result = ""
 for coodinate in coodinates:
