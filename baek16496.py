46f250efd25018dec57074a3a59866d0ff9ad138
김령교 <horizonspluto@gmail.com>
2024-01-17 20:22:43 +0900

    [#69]큰 수 만들기(16496번). bubble sort처럼 구현. 86%에서 틀림

---
+++ baek16496.py
@@ -0,0 +1,14 @@
+n = int(input())
+numList = input().split(" ")
+
+i = 0
+while i < len(numList):
+    j = 0
+    while j < len(numList)-1 - i:
+        if int(numList[j] + numList[j+1]) < int(numList[j+1] + numList[j]):
+            numList[j], numList[j+1] = numList[j+1], numList[j]
+        j += 1
+    i += 1
+
+for num in numList:
+    print(num, end="")
