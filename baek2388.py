e446d2702ef0a782dd6a9daf75e83b7c4406d53c
김령교 <horizonspluto@gmail.com>
2024-01-18 14:06:11 +0900

    [#71]블록 쌓기(2388번). 중복된 코드를 함수화함. maxCount logic의 오류를 고침. pypy로 19%에서 IndexError 남

--- baek2388.py
+++ baek2388.py
@@ -1,42 +1,49 @@
 N, M = map(int, input().split(" "))
 
+def binarySearch(lst, item):
+    start = 0
+    end = len(lst) - 1
+    mid = (start + end) // 2
+    while lst[mid] != item:
+        now = lst[mid]
+        if item < now:
+            end = mid - 1
+        elif item > now:
+            start = mid + 1
+        mid = (start + end) // 2
+    return mid
+
+def coordinateCompression(sortedCoordinates):
+    compressionChart = sorted(list(set(sortedCoordinates)))
+    compressedCoordinates = []
+    for coordinate in sortedCoordinates:
+        compressedCoordinates.append(binarySearch(sortedCoordinates, coordinate))
+    return compressionChart, compressedCoordinates
+
+def cumulateSum(compressionChart, compressedCoordinates):
+    prefixSum = []
+    for _ in range(len(compressionChart)):
+        prefixSum.append(0)
+
+    i = len(compressionChart) - 1
+    cumulSum = 0
+    while i >= 0:
+        cumulSum += 1
+        prefixSum[compressedCoordinates[i]] = cumulSum
+        i -= 1
+    return prefixSum
+
 front = []
 for _ in range(N):
     front.append(int(input()))
 front.sort()
-frontChart = sorted(list(set(front)))
-compressFront = []
-for bar in front:
-    start = 0
-    end = len(frontChart) - 1
-    mid = (start + end) // 2
-    while frontChart[mid] != bar:
-        now = frontChart[mid]
-        if bar < now:
-            end = mid-1
-        elif bar > now:
-            start = mid+1
-        mid = (start + end) // 2
-    compressFront.append(mid)
+frontChart, compressFront = coordinateCompression(front)
 
 side = []
 for _ in range(M):
     side.append(int(input()))
 side.sort()
-sideChart = sorted(list(set(side)))
-compressSide = []
-for bar in side:
-    start = 0
-    end = len(sideChart) - 1
-    mid = (start + end) // 2
-    while sideChart[mid] != bar:
-        now = sideChart[mid]
-        if bar < now:
-            end = mid-1
-        elif bar > now:
-            start = mid+1
-        mid = (start + end) // 2
-    compressSide.append(mid)
+sideChart, compressSide = coordinateCompression(side)
 
 if front[-1] == side[-1]:
     # min count part
@@ -62,51 +69,36 @@ if front[-1] == side[-1]:
             minCount += frontBar
             if i < len(front) - 1:
                 i += 1
-
             if j < len(side) - 1:
                 j += 1
 
-    print(minCount)
+    print(minCount, end=" ")
 
     # max count part
-    frontSum = []
-    for _ in range(len(frontChart)):
-        frontSum.append(0)
-    i = len(frontChart) - 1
-    cumulSum = 0
-    while i >= 0:
-        cumulSum += 1
-        frontSum[compressFront[i]] = cumulSum
-        i -= 1
-
-    sideSum = []
-    for _ in range(len(sideChart)):
-        sideSum.append(0)
-    j = len(sideChart) - 1
-    cumulSum = 0
-    while j >= 0:
-        cumulSum += 1
-        sideSum[compressSide[j]] = cumulSum
-        j -= 1
+    frontSum = cumulateSum(frontChart, compressFront)
+    sideSum = cumulateSum(sideChart, compressSide)
 
     maxCount = 0
     lastI = None
     lastJ = None
+    lastHighBar = 0
     i = 0
     j = 0
     while lastI != i or lastJ != j:
-        frontBar = frontChart[i]
-        sideBar = sideChart[j]
+        frontBar = front[i]
+        sideBar = side[j]
         frontCount = frontSum[i]
         sideCount = sideSum[j]
         lastI = i
         lastJ = j
         if frontBar < sideBar:
-            maxCount += (frontBar-frontChart[i]+1) * frontCount * sideCount
+            maxCount += (frontBar-lastHighBar) * frontCount * sideCount
+            lastHighBar = frontBar
             if i < len(frontSum) - 1:
                 i += 1
         elif frontBar > sideBar:
-            maxCount += (sideBar-sideChart[j]+1) * frontCount * sideCount
+            maxCount += (sideBar-lastHighBar) * frontCount * sideCount
+            lastHighBar = sideBar
             if j < len(sideSum) - 1:
                 j += 1
         else:
@@ -116,6 +108,7 @@ if front[-1] == side[-1]:
 
             if j < len(sideSum) - 1:
                 j += 1
+            lastHighBar = frontBar
 
     print(maxCount)
 else:
