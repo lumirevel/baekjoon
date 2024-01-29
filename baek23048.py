10119d6e1da47c6cf22487b69a5b10cb86222fba
김령교 <horizonspluto@gmail.com>
2024-01-29 20:33:44 +0900

    [#93]자연수 색칠하기(23048번)를 해결함

--- baek23048.py
+++ baek23048.py
@@ -1,17 +1,24 @@
 N = int(input())
 
+maxColor = 1
 primeList = []
-for num in range(1, N+1):
-    if num == 1:
-        print(1, end=" ")
-    else:
-        isPrime = True
-        i = 0
-        while i < len(primeList) and isPrime and primeList[i]*primeList[i] <= num:
-            if num % primeList[i] == 0:
-                isPrime = False
-                print(i+2, end=" ")
-            i += 1
-        if isPrime:
-            primeList.append(num)
-            print(len(primeList)+1, end=" ")
+coloredList = [1]
+for num in range(2, N+1):
+    isPrime = True
+    i = 0
+    while i < len(primeList) and isPrime and primeList[i]*primeList[i] <= num:
+        if num % primeList[i] == 0:
+            isPrime = False
+            color = i+2
+            maxColor = max(maxColor, color)
+            coloredList.append(color)
+        i += 1
+    if isPrime:
+        primeList.append(num)
+        color = len(primeList)+1
+        maxColor = max(maxColor, color)
+        coloredList.append(color)
+
+print(maxColor)
+for color in coloredList:
+    print(color, end=" ")
