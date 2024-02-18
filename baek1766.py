dbd64e1519eee03b62a9df42f149aafc0c77d443
김령교 <horizonspluto@gmail.com>
2024-02-18 23:12:20 +0900

    [#144]문제집(1766번)을 해결함. Topology Sort Algorithm을 다시 공부하며 아름다운 구현을 발견하였고, 그것을 토대로 작성하여 맞췄다

--- baek1766.py
+++ baek1766.py
@@ -5,33 +5,27 @@ setrecursionlimit(100005)
 
 N, M = map(int, stdin.readline().split(" "))
 adjList = []
-reverseAdjList = []
 for _ in range(N):
     adjList.append([])
-    reverseAdjList.append([])
+inDegreeList = [0] * N
 for _ in range(M):
     A, B = map(int, stdin.readline().split(" "))
     adjList[A-1].append(B-1)
-    reverseAdjList[B-1].append(A-1)
+    inDegreeList[B-1] += 1
 
 
-for subList in adjList:
-    subList.sort()
-sortedList = []
-doneParentCountList = [0] * N
+waitingList = PriorityQueue()
 for i in range(N):
-    waitingList = PriorityQueue()
-    waitingList.put(i)
-    while waitingList.queue:
-        now = waitingList.get()
-        if doneParentCountList[now] == len(reverseAdjList[now]):
-            # 자식에게서 다시 자신이 나오려면 cycle이 존재해야하는데 이는 Topology Sort에 모순이다.
-            sortedList.append(now+1)
-            for child in adjList[now]:
-                # 이미 sortedList에 추가된 자식이 이곳에 도착하려면 자식의 처리 안 된 parent가 존재하면 안되는데 방금 직전까지 존재했으므로 모순이다.
-                doneParentCountList[child] += 1
-                if child < i:
-                    waitingList.put(child)
+    if inDegreeList[i] == 0:
+        waitingList.put(i)
+sortedList = []
+while waitingList.queue:
+    now = waitingList.get()
+    sortedList.append(now+1)
+    for child in adjList[now]:
+        inDegreeList[child] -= 1
+        if inDegreeList[child] == 0:
+            waitingList.put(child)
 
 
 for number in sortedList:
