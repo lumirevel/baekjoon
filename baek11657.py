1cc543d8f48f6a4f25610158f6e64f7210637a03
김령교 <horizonspluto@gmail.com>
2024-02-21 04:01:52 +0900

    [#152]타임머신(11657번). 이상하게 52%에서 틀림

---
+++ baek11657.py
@@ -0,0 +1,27 @@
+N, M = map(int, input().split(" "))
+edge = []
+for _ in range(M):
+    A, B, C = map(int, input().split(" "))
+    edge.append((A-1, B-1, C))
+
+
+shortestList = [float("inf")] * N
+def bellmanFord(start = 0):
+    shortestList[start] = 0
+
+    for _ in range(N-1):
+        for u, v, w in edge:
+            newDistance = shortestList[u] + w
+            if newDistance < shortestList[v]:
+                shortestList[v] = newDistance
+    for u, v, w in edge:
+        if shortestList[u] + w < shortestList[v]:
+            return False
+    return True
+
+
+if bellmanFord():
+    for shortest in shortestList[1:]:
+        print(shortest)
+else:
+    print(-1)
