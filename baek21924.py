97af8f2bd88a6ef197325416f27b4a8e78406f81
김령교 <horizonspluto@gmail.com>
2024-02-21 07:08:22 +0900

    [#156]도시 건설(21924번). prim으로 잘 안돼서 kruskal로 구현을 바꿨으나 여전히 2%에서 python으로는 시간초과, pypy로는 런타임에러 남

--- baek21924.py
+++ baek21924.py
@@ -2,39 +2,56 @@ from sys import stdin
 from queue import PriorityQueue
 N, M = map(int, stdin.readline().split(" "))
 totalCost = 0
-adjList = []
-for _ in range(N):
-    adjList.append([])
+edgeList = PriorityQueue()
 for _ in range(M):
     a, b, c = map(int, stdin.readline().split(" "))
-    adjList[a-1].append((c, b-1))
-    adjList[b-1].append((c, a-1))
+    edgeList.put((c, a-1, b-1))
+    edgeList.put((c, b-1, a-1))
     totalCost += c
 
 
-def prim(now = 0):
-    visited = [False] * N
+class DisjointSet:
+    class Item:
+        def __init__(self):
+            self.p = self
+            self.rank = 0
 
-    waiting = PriorityQueue()
-    waiting.put((0, now))
-    edgeCount = 0
-    totalW = 0
-    while waiting.queue:
-        w, now = waiting.get()
-        if not visited[now]:
-            visited[now] = True
-            edgeCount += 1
-            totalW += w
-            for w, v in adjList[now]:
-                if not visited[v]:
-                    waiting.put((w, v))
-    if edgeCount == N:
-        return totalW
-    return -1
-saleW = prim()
+    def __init__(self, size):
+        self.size = size
+        self.elementList = []
+        for _ in range(size):
+            self.elementList.append(DisjointSet.Item())
+
+    def linkItem(self, x, y):
+        if x.rank > y.rank:
+            y.p = x
+        else:
+            x.p = y
+            if x.rank == y.rank:
+                y.rank = y.rank + 1
+
+    def union(self, x, y):
+        self.linkItem(self.findSet(x), self.findSet(y))
+
+    def findSet(self, x):
+        return self.findSetItem(self.elementList[x])
+    def findSetItem(self, x):
+        if x.p != x:
+            x.p = self.findSetItem(x.p)
+        return x.p
+
+edgeCount = 0
+saleW = 0
+check = DisjointSet(N)
+while edgeList.queue:
+    w, u, v = edgeList.get()
+    if check.findSet(u) != check.findSet(v):
+        edgeCount += 1
+        saleW += w
+        check.union(u, v)
 
 
-if saleW != -1:
+if edgeCount == N-1:
     print(totalCost-saleW)
 else:
     print(-1)
