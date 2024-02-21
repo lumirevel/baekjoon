dee95e93f16cd76cb11386a4fcfd0bc1ec6f7571
김령교 <horizonspluto@gmail.com>
2024-02-21 21:32:26 +0900

    [#166]퇴사 2(15486번)를 해결함

--- baek15486.py
+++ baek15486.py
@@ -1,25 +1,26 @@
 from sys import stdin
 N = int(stdin.readline())
-TP = []
+TPList = []
+for _ in range(N):
+    TPList.append([])
 for i in range(N):
     Ti, Pi = map(int, stdin.readline().split(" "))
-    TP.append((i, i+Ti-1, Pi))
+    end = i+Ti-1
+    if end < len(TPList):
+        TPList[end].append((i, Pi))
 
 
 # DP[i]는 i+1에 퇴사할 때의 최대 값. 즉, i+1부터 회의 잡기 가능
-TP.sort(key=lambda x:x[1])
-beforeEnd = None
 DP = [0] * N
-for begin, end, P in TP:
-    if beforeEnd is not None:
-        for i in range(beforeEnd, min(N, end)-1):
-            DP[i+1] = max(DP[i+1], DP[i])
-    beforeEnd = end
-    if end < len(DP):
+for i, counselList in enumerate(TPList):
+    if i != 0:
+        DP[i] = DP[i-1]
+    for counsel in counselList:
+        begin, P = counsel
         if begin == 0:
-            DP[end] = max(DP[end], P)
+            DP[i] = max(DP[i], P)
         else:
-            DP[end] = max(DP[end], DP[begin-1] + P)
+            DP[i] = max(DP[i], DP[begin-1] + P)
 
 
 print(DP[-1])
